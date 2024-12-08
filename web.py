659cb41ade122384170c4b31137f05415d499dac
Denys <hetfield228@gmail.com>
2024-12-08 22:37:49 +0200

    Fun

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import functions
 import os
+import time
 
 if not os.path.exists("todos.txt"):
     with open("todos.txt", 'w') as file:
@@ -8,23 +9,38 @@ if not os.path.exists("todos.txt"):
 
 todos_list = functions.read_file()
 
-st.video("cat.mp4", autoplay=True)
+enable = st.checkbox("Enable camera", key="camera_enabled")
 
-def add_todo():
-    new_todo = st.session_state["input_todo"] + '\n'
+st.camera_input(label="Take a Photo and make Video from it!",
+                disabled=not enable,
+                label_visibility="visible",
+                key="take_photo")
 
-    todos_list.append(new_todo)
-    functions.write_file(todos_list)
+picture = st.session_state["take_photo"]
 
-st.title("TODO List")
+if picture:
+    st.image(picture)
+    st.button("Make video", key="make_video", use_container_width=True)
+    if st.session_state["make_video"]:
+        time.sleep(3)
+        del st.session_state["take_photo"]
+        st.video("cat.mp4", loop=True)
 
-for index, todo in enumerate(todos_list):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos_list.pop(index)
-        functions.write_file(todos_list)
-        del st.session_state[todo]
-        st.rerun()
-
-st.text_input(label="Enter a todo:", placeholder="Add a new todo...", label_visibility="collapsed",
-              on_change=add_todo, key="input_todo")
+# def add_todo():
+#     new_todo = st.session_state["input_todo"] + '\n'
+#
+#     todos_list.append(new_todo)
+#     functions.write_file(todos_list)
+#
+# st.title("TODO List")
+#
+# for index, todo in enumerate(todos_list):
+#     checkbox = st.checkbox(todo, key=todo)
+#     if checkbox:
+#         todos_list.pop(index)
+#         functions.write_file(todos_list)
+#         del st.session_state[todo]
+#         st.rerun()
+#
+# st.text_input(label="Enter a todo:", placeholder="Add a new todo...", label_visibility="collapsed",
+#               on_change=add_todo, key="input_todo")
