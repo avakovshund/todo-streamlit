2df8c9e72ee422ecf8280c6c47ddbdef951795de
Denys <hetfield228@gmail.com>
2024-12-08 22:42:50 +0200

    Fun

--- web.py
+++ web.py
@@ -25,22 +25,3 @@ if picture:
         time.sleep(3)
         del st.session_state["take_photo"]
         st.video("cat.mp4", loop=True)
-
-# def add_todo():
-#     new_todo = st.session_state["input_todo"] + '\n'
-#
-#     todos_list.append(new_todo)
-#     functions.write_file(todos_list)
-#
-# st.title("TODO List")
-#
-# for index, todo in enumerate(todos_list):
-#     checkbox = st.checkbox(todo, key=todo)
-#     if checkbox:
-#         todos_list.pop(index)
-#         functions.write_file(todos_list)
-#         del st.session_state[todo]
-#         st.rerun()
-#
-# st.text_input(label="Enter a todo:", placeholder="Add a new todo...", label_visibility="collapsed",
-#               on_change=add_todo, key="input_todo")
